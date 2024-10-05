49fc1a079a29a143ba3f74dd491b5697e59d47f5
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-10-05 12:10:29 +0500

    123

--- CleanCode&CodeStyle7/Program.cs
+++ CleanCode&CodeStyle7/Program.cs
@@ -1,67 +1,70 @@
 ﻿using System;
 
-public class Player
+namespace CleanCode_CodeStyle7
 {
-    private Movement _movement;
-    private Weapon _weapon;
-    private string _name;
-    private int _age;       
-
-    public Player(Movement movement, Weapon weapon, string name, int age)
+    public class Player
     {
-        _movement = movement ?? throw new ArgumentNullException(nameof(movement));
-        _weapon = weapon ?? throw new ArgumentNullException(nameof(weapon));
-        _name = name != string.Empty ? name : throw new ArgumentNullException(nameof(name));
-        _age = age > 0 ? age : throw new ArgumentOutOfRangeException(nameof(age));
+        private Movement _movement;
+        private Weapon _weapon;
+        private string _name;
+        private int _age;
+
+        public Player(Movement movement, Weapon weapon, string name, int age)
+        {
+            _movement = movement ?? throw new ArgumentNullException(nameof(movement));
+            _weapon = weapon ?? throw new ArgumentNullException(nameof(weapon));
+            _name = name != string.Empty ? name : throw new ArgumentNullException(nameof(name));
+            _age = age > 0 ? age : throw new ArgumentOutOfRangeException(nameof(age));    
+        }
+
+        public void Move() => 
+            _movement.Move();
+
+        public bool IsReloading() => 
+            _weapon.IsReloading();
+
+        public void Attack() => 
+            _weapon.Attack();
     }
 
+    public class Movement
+    {
+        public Movement(float directionHorizontal, float directionVertical, float speed)
+        {
+            DirectionHorizontal = directionHorizontal > 0 ? directionHorizontal : throw new ArgumentOutOfRangeException(nameof(directionHorizontal));
+            DirectionVertical = directionVertical > 0 ? directionVertical : throw new ArgumentOutOfRangeException(nameof(directionVertical));
+            Speed = speed >= 0 ? speed : throw new ArgumentOutOfRangeException(nameof(speed));
+        }
+
+        public float DirectionHorizontal { get; }
+        public float DirectionVertical { get; }
+        public float Speed { get; }
+
+        public void Move()
+        {
+            //Do move
+        }
+    }
+
+    public class Weapon
+    {
+        public Weapon(float cooldown, int damage)
+        {
+            Cooldown = cooldown > 0 ? cooldown : throw new ArgumentOutOfRangeException(nameof(cooldown));
+            Damage = damage > 0 ? damage : throw new ArgumentOutOfRangeException(nameof(damage));
+        }
+
+        public float Cooldown { get; }
+        public int Damage { get; }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            //attack
+        }
+    }
 }
-
-public class Movement
-{
-    private float _directionHorizontal;
-    private float _directionVertical;
-    private float _speed;
-
-    public Movement(float directionHorizontal, float directionVertical, float speed)
-    {
-        _directionHorizontal = directionHorizontal;
-        _directionVertical = directionVertical;
-        _speed = speed;
-    }
-
-    public float DirectionHorizontal => _directionHorizontal;
-    public float DirectionVertical => _directionVertical;
-    public float Speed => _speed;
-
-    public void Move()
-    {
-        //Do move
-    }
-}
-
-public class Weapon
-{
-    private float _cooldown;
-    private int _damage;
-
-    public Weapon(float cooldown, int damage)
-    {
-        _cooldown = cooldown;
-        _damage = damage;
-    }
-
-    public float Cooldown => _cooldown;
-    public int Damage => _damage;
-
-    public bool IsReloading()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Attack()
-    {        
-        //attack
-    }
-}
-
